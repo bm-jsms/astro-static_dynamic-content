c43dd4a6c55c8b7c7863d5c9cf4d326959eae9f5
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-09-18 19:28:51 -0500

    🎉 [create] Favorite Pokemons - main component

---
+++ src/components/pokemon/FavoritePokemons.tsx
@@ -0,0 +1,20 @@
+import type { FavoritePokemon } from '@interfaces/favoritePokemon';
+import { For, createSignal } from 'solid-js';
+
+const getLocalStoragePokemons = (): FavoritePokemon[] => {
+	const favoritePokemons = JSON.parse(
+		localStorage.getItem('favorites') ?? '[]',
+	);
+
+	return favoritePokemons;
+};
+
+export const FavoritePokemons = () => {
+	const [pokemons, setPokemons] = createSignal(getLocalStoragePokemons());
+
+	return (
+		<div class='grid grid-cols-2 sm:grid-cols-4'>
+			<For each={pokemons()}>{(pokemon) => <h2>{pokemon.name}</h2>}</For>
+		</div>
+	);
+};
